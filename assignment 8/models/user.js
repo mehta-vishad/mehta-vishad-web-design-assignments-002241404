af8237fdfc59098b147bd1b584c381d6cf1623c5
vishadm <vishadm00@gmail.com>
2023-11-11 04:47:48 -0500

    assignment 8 added

---
+++ assignment 8/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+//userSchema.index({ email: 1 }, { unique: true });
+module.exports = mongoose.model('User', userSchema)
